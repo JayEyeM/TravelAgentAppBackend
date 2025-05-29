1299a93950b7943f8d45d6e05ff05c7bb709db2d
JayEye <136415777+JayEyeM@users.noreply.github.com>
2025-05-29 20:38:00 -0230

    Basic Commissions Calculator Endpoints
    HandyTools Basic Commission Calculator endpoints.

--- src/handyTools/BasicCommissionCalculator/router.ts
+++ src/handyTools/BasicCommissionCalculator/router.ts
@@ -1,8 +1,11 @@
-// File: src/handyTools/BasicCommissionCalculator/handlers.ts
+// File: src/handyTools/BasicCommissionCalculator/router.ts
 
 import { Hono } from "hono";
-import { createCommission } from "../BasicCommissionCalculator/handlers";
+import { createCommission, readCommissions, updateCommission, deleteCommission } from "../BasicCommissionCalculator/handlers";
 
 export const BasicCommissionRouter = new Hono();
 
 BasicCommissionRouter.post("/", ...createCommission);
+BasicCommissionRouter.get("/:userId", ...readCommissions);
+BasicCommissionRouter.patch("/:id", ...updateCommission);
+BasicCommissionRouter.delete("/:id", ...deleteCommission);
--- src/handyTools/utils/caseConverter.ts
+++ src/handyTools/utils/caseConverter.ts
@@ -17,3 +17,22 @@ export const convertKeysToSnakeCase = (obj: any): any => {
   }
   return obj;
 };
+
+
+// Converts snake_case string to camelCase string
+export const snakeToCamel = (str: string): string =>
+  str.replace(/_([a-z])/g, (_, letter) => letter.toUpperCase());
+
+// Converts an object with snake_case keys to camelCase keys
+export const convertKeysToCamelCase = (obj: any): any => {
+  if (Array.isArray(obj)) {
+    return obj.map(v => convertKeysToCamelCase(v));
+  } else if (obj !== null && obj.constructor === Object) {
+    return Object.keys(obj).reduce((result, key) => {
+      const camelKey = snakeToCamel(key);
+      result[camelKey] = convertKeysToCamelCase(obj[key]);
+      return result;
+    }, {} as any);
+  }
+  return obj;
+}
